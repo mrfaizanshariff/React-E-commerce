a78975844882b4fb6302319676b262d52eb25861
mrfaizanshariff <mrfaizanshariff@gmail.com>
2021-09-24 18:15:22 +0530

    menu-items Styling is complete

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.styles.scss";
+import MenuContainer from "../../components/menu-container.component/menu-container.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <MenuContainer />
+  </div>
+);
+export default HomePage;
