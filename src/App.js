7981abec16e813803cc77b22f48f03e4fb355283
mrfaizanshariff <mrfaizanshariff@gmail.com>
2021-09-25 23:09:06 +0530

    added Header Comp, links to home,shop and contact pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header/header";
 const HatsPage = () => (
   <div>
     <h1>HAts PAge</h1>
@@ -12,8 +12,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
+      <Header />
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </switch>
     </div>
   );
 }
